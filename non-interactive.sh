f0b7f55a41bec1c2488b7e645f9ee8f0d7b3d08a
SamW <mail@sampersand.me>
2024-10-19 21:52:27 -0700

    added mv-safe and trash, but they require some updating to be sh

--- non-interactive.sh
+++ non-interactive.sh
@@ -1,5 +1,7 @@
 export SampShell_HOME="${SampShell_HOME:-"$HOME/.sampshell"}"
 export SampShell_EDITOR="${SampShell_EDITOR:-sublime4}"
+export SampShell_TRASHDIR="${SampShell_TRASHDIR:-"$HOME/.Trash/.sampshell-trash"}"
+export SampShell_TMPDIR="${SampShell_TMPDIR:-"$HOME/tmp"}"
 
 export PATH="$SampShell_HOME/bin:$PATH"
 
--- shared/changing-dirs.sh
+++ shared/changing-dirs.sh
@@ -3,6 +3,8 @@ cdd () {
 	cd "$(dirname "$1")"
 }
 
+alias cdtmp='cd "$SampShell_TMPDIR"'
+
 # Aliases for going up directories
 alias ..='cd ..'
 alias ...='cd ../..'
