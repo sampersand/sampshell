ee59ef03295da134f88118db4fc9035ff325f171
SamW <mail@sampersand.me>
2024-11-05 19:05:38 -0800

    add -v to mv-safe to have debug output

--- zsh/interactive/aliases.zsh
+++ zsh/interactive/aliases.zsh
@@ -2,3 +2,6 @@ alias d=dirs
 alias '$= ' # Make sure an `$` at the beginning of a line will be ignored.
 
 eval "$(alias -L ls)hGb" # alias options which i know exist elsewhere
+
+alias t=trash
+alias m='mv-safe -v'
