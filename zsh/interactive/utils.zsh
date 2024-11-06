e65d97388339671a66afc8d12c967a8aedda6c07
SamW <mail@sampersand.me>
2024-11-05 19:13:32 -0800

    update zreload

--- zsh/interactive/utils.zsh
+++ zsh/interactive/utils.zsh
@@ -1,7 +1,12 @@
-clean-zsh () {
+function clean-zsh {
 	env -i "$@" ${TERM:+TERM="$TERM"} =zsh -f
 }
 
-clean-sh () {
+function clean-sh {
 	clean_sh ENV=${SampShell_ROOTDIR?}/interactive.sh
+}
+
+function zreload {
+	[ -e "${ZDOTDIR:-$HOME}/.zshenv" ] && source "${ZDOTDIR:-$HOME}/.zshenv"
+	[ -e "${ZDOTDIR:-$HOME}/.zshrc" ]  && source "${ZDOTDIR:-$HOME}/.zshrc"
 }
