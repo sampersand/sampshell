d3271c1e97ddc1476b8b9bbe4e2445e0929423bb
SamW <mail@sampersand.me>
2024-11-07 10:11:36 -0800

    add PUSHD_IGNORE_DUPS

--- zsh/interactive/changing-dirs.zsh
+++ zsh/interactive/changing-dirs.zsh
@@ -3,6 +3,7 @@ setopt AUTO_PUSHD        # All `cd`s push directories
 setopt CHASE_LINKS       # Always resolve paths as their absolute paths.
 setopt CDABLE_VARS       # Able to cd to directory vars
 setopt PUSHD_SILENT      # pushd no longer prints things out; So annoying, just use `dir` if needed
+setopt PUSHD_IGNORE_DUPS # don't put multiple of the same dir on the stack
 
 # setopt AUTO_NAME_DIRS  # kinda iffy, cds to any variable
 
