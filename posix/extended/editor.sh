ad6bc74167057000203405140ad3ffd5ad68fac3
SamW <mail@sampersand.me>
2024-11-08 23:57:17 -0800

    adding history

--- posix/extended/editor.sh
+++ posix/extended/editor.sh
@@ -1,10 +1,7 @@
-## Setup Sublime Text commands, unless it's disabled.
-if [ -z "${SampShell_no_subl-}" ]  ; then
-	alias s=subl
-	alias ss=ssubl
-	alias ssubl='subl --create'
+alias s=subl
+alias ss=ssubl
+alias ssubl='subl --create'
 
-	## Spellchecks
-	alias sbul=subl
-	alias ssbul=ssubl
-fi
+## Spellchecks
+alias sbul=subl
+alias ssbul=ssubl
