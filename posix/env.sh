d9505b7967b5e36bb88f5d007dcdf8a3ba2056b9
SamW <mail@sampersand.me>
2024-11-10 15:09:54 -0800

    Make parallelize_it not need local

--- posix/env.sh
+++ posix/env.sh
@@ -179,64 +179,66 @@ SampShell_cdd () {
 }
 
 ## Parallelize a function by making a new job once per argument given
-# Oh boy, it's far too annoying making this without `local`
-if SampShell_command_exists local; then
-	SampShell_parallelize_it () {
-		local expand fn skipchr
+SampShell_parallelize_it () {
+	# Support for when the shell is ZSH, when we explicitly have `-e`.
+	[ -n "$ZSH_VERSION" ] && setopt LOCAL_OPTIONS GLOB_SUBST SH_WORD_SPLIT
 
-		while :; do
-			case "$1" in
-				-h)
-					cat <<-EOS
-						usage: $0 [options] [--] fn [args ...]
-						options:
-						   -e      use expansion on 'args'
-						   -X      don't have a skipchar; overrides -x
-						   -x[CHR] set the skipchar; if omitted defaults to 'x'
-						   -fFUNC  sets the function to execute; if given, omit 'fn'
-						           after '--'.
-						This command executes 'fn' once for each arg as background job
-					EOS
-					return 64 ;;
-				-e) expand=1 ;;
-				-X) skipchr= ;;
-				-x) skipchr="${1#-x}"; skipchr="${skipchr:-x}" ;;
-				-f*)
-					fn="${1#-f}"
-					if [ -z "$fn" ]; then fn="$2"; shift; fi ;;
-				--) break ;;
-				*) break ;;
-			esac
-			shift
-		done
+	SampShell_scratch=
+	if [ "${1-}" = -- ]; then
+		SampShell_scratch=0
+		shift
+	elif [ "${1-}" = -e ]; then
+		SampShell_scratch=1
+		shift
+	fi
 
-		if [ "$1" = "--" ]; then
-			shift
-		elif [ -z "$fn" ]; then
-			fn="$1"
-			shift
+	# Only support `-h` if `--` wasn't given
+	if [ -z "${1-}" ] || { [ "$SampShell_scratch" != 0 ] && [ "$1" = -h ]; }; then
+		echo "usage: SampShell_parallelize_it [-e] [--] fn [args ...]" >&2
+		echo "(-e does shell expansion on args; without it, args are quoted)"
+		unset -v SampShell_scratch
+		return 1
+	fi
+
+	if ! command -v "$1" >/dev/null 2>&1; then
+		printf 'SampShell_parallelize_it: fn is not executable: %s\n' "$1" >&2
+		return 1
+	fi
+
+	[ "$SampShell_scratch" = 0 ] && SampShell_scratch=
+
+	while [ "$#" -gt 1 ]; do
+		# If we're expanding...
+		if [ -n "${SampShell_scratch-}" ]; then
+			# Unset `SampShell_scratch` so the child process doesn't see it
+			unset -v SampShell_scratch
+
+			# Run the function
+			"$1" $2 &
+
+			# Remove argument #2
+			SampShell_scratch=$1
+			shift 2
+			set -- "$SampShell_scratch" "$@"
+
+			# Set it so we'll go into this block next time.
+			SampShell_scratch=1
+		else
+			# Run the function
+			"$1" "$2" &
+
+			# Remove argument #2
+			SampShell_scratch=$1
+			shift 2
+			set -- "$SampShell_scratch" "$@"
+
+			# unset it so won't run the expand block.
+			unset -v SampShell_scratch
 		fi
+	done
 
-		if [ -z "$fn" ]; then
-			echo "no function given!" >&2
-			return 1
-		elif ! type "$fn" >/dev/null 2>&1; then
-			echo "function '$fn' is not executable" >&2
-			return 2
-		fi
-
-		until [ "$#" = 0 ]; do
-			if [ -n "$skipchr" ] && [ "$skipchr" = "$2" ]; then
-				shift
-			elif [ -n "$expand" ]; then
-				"$fn" $1 &
-			else
-				"$fn" "$1" &
-			fi
-			shift
-		done
-	}
-fi
+	unset -v SampShell_scratch
+}
 
 
 ################################################################################
