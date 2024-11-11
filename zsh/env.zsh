e4c3710118a6f6da138b8cf86f394d42c59114ac
SamW <mail@sampersand.me>
2024-11-10 22:07:11 -0800

    move disable-weirdness into zsh/env.zsh

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -2,10 +2,6 @@ typeset -Uxg path # make sure `path` is unique, and then export it
 
 path+=${0:P:h}/bin
 
-for file in "${0:P:h}"/non-interactive/*.zsh; do
-	source $file
-done
-
 setopt EXTENDED_GLOB          # Add additional glob syntax in zsh
 setopt NO_IGNORE_CLOSE_BRACES # Allow `}` to also be a `;`
 setopt GLOB_STAR_SHORT        # **.c is an alias for **/*.c
@@ -20,3 +16,12 @@ function SampShell-debug {
 function {SampShell-,}undebug {
 	SampShell_undebug && setopt NO_{SOURCE_TRACE,UNSET,WARN_CREATE_GLOBAL,WARN_NESTED_VAR}
 }
+
+setopt NO_KSH_GLOB
+setopt NO_PATH_DIRS # don't searrch subdirs of `PATH` for commands if `a/b` is specified.
+setopt SHORT_LOOPS # Why would this be disabled? Also `SHORT_REPEAT`?
+setopt NO_GLOB_SUBST # huzzah, this is osmething we shouldn't have anyumore!
+setopt NO_ALIAS_FUNC_DEF
+false && setopt CHASE_LINKS # TODO: would this be bad?
+false && setopt NO_POSIX_CD       # Disable posix-cd features
+false && setopt SH_GLOB GLOBAL_EXPORT NOMATCH
