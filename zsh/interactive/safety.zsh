a8f096fc769e5b6b64221d4dd1315af9c02e4797
SamW <mail@sampersand.me>
2024-11-12 16:46:52 -0800

    remove RM_STAR_WAIT

--- zsh/interactive/safety.zsh
+++ zsh/interactive/safety.zsh
@@ -4,6 +4,5 @@
 setopt NO_CLOBBER        # Should already be set, but just in case.
 setopt CLOBBER_EMPTY     # However, you can clobber empty files.
 setopt NO_RM_STAR_SILENT # In case it's accidentally unset, force `rm *` to ask for confirmation
-setopt RM_STAR_WAIT      # Accidentally do `rm *`, wait 10s before doing it.
 
 # Note that the rest of the config for safety (eg `alias rm='rm -i`) are in `posix/interactive.sh`
