b80d9bac574c9c0e808dc474e1279be375a9e206
SamW <mail@sampersand.me>
2024-11-19 16:49:12 -0800

    make utils.zsh, idk if i want

--- zsh/interactive/utils.zsh
+++ zsh/interactive/utils.zsh
@@ -46,7 +46,6 @@ puts ($*.empty? ? $stdin.map{_1.chomp.split} : [$*])
 	.map{_1.map(&:hex).pack('C*')}
 RUBY
 
-
 function enable-wifi { networksetup -setairportpower en0 on }
 function disable-wifi { networksetup -setairportpower en0 off }
 function toggle-wifi { disable-wifi; sleep 2; enable-wifi }
