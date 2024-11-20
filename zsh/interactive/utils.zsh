4fc408a2a217e6e1c0afc2338980bbfb58dc8cd2
SamW <mail@sampersand.me>
2024-11-19 16:47:24 -0800

    moved utils

--- zsh/interactive/utils.zsh
+++ zsh/interactive/utils.zsh
@@ -34,4 +34,19 @@ function clzsh { clean-zsh --none -- -fd $@ } # Don't set SampShell variables, o
 rd () { builtin rm -f -- ${1:?need a dir}/.DS_Store && builtin rmdir -- $1 }
 md () { builtin mkdir -p -- "${1:?missing a directory}" && builtin cd -- "$1" }
 
-source ${0:P:h}/extended/utils.zsh
+# utility functions and what have you that I've accumulated over the years
+chr () ruby -- /dev/fd/3 $@ 3<<'RUBY'
+puts ([]==$*?$stdin.map(&:chomp):$*).map{|w|w.bytes.map{_1.to_s 16}.join(?\s)}
+RUBY
+# $*.empty? and $*.replace $stdin.map(&:chomp)
+# puts $*.map{|w|w.bytes.map{|b|b.to_s 16}.join(?\s)}
+
+ord () ruby -- /dev/fd/3 $@ 3<<'RUBY'
+puts ($*.empty? ? $stdin.map{_1.chomp.split} : [$*])
+	.map{_1.map(&:hex).pack('C*')}
+RUBY
+
+
+function enable-wifi { networksetup -setairportpower en0 on }
+function disable-wifi { networksetup -setairportpower en0 off }
+function toggle-wifi { disable-wifi; sleep 2; enable-wifi }
