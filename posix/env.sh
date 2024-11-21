0b7ffec9133d7bf5b59aa8f5b6cbe7247bf64ee6
SamW <mail@sampersand.me>
2024-11-20 22:23:08 -0800

    comment fix

--- posix/env.sh
+++ posix/env.sh
@@ -164,7 +164,7 @@ SampShell_debug () {
    export SampShell_VERBOSE=1 SampShell_TRACE=1 && set -o xtrace -o verbose
 }
 
-## Enables debugging mode
+## Disables debugging mode
 # This disables all of SampShell_debug's debugging capabilities, as well as the
 # current shell; it's expected that this is overwritten in per-shell config.
 SampShell_undebug () {
