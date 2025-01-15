4ee1ffe281ac77e0eb7b373db84eed3435f18165
SamW <mail@sampersand.me>
2025-01-15 11:51:57 -0800

    noneed for reset-formatting-after-prompt

--- zsh/prompt/main.zsh
+++ zsh/prompt/main.zsh
@@ -1,8 +1,11 @@
 source ${0:P:h}/fix-spaces-after-eol-mark-macos.zsh
-source ${0:P:h}/reset-formatting-after-prompt.zsh
 source ${0:P:h}/prompt-widgets.zsh
 make-ps1
 unfunction make-prompt
+# Create the prompt with default values
 source ${0:P:h}/ps1.zsh
 
-# Create the prompt with default values
+
+## Special ZSH variable that is printed after we enter a command; We use it to make sure we reset
+# the colouring in case the prompt gets screwed up somehow
+POSTEDIT=$(print -nP %b%u%s%f)
