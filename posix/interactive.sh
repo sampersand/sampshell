d1e279d44b3e712d0b80991d898fb246dba2af85
SamW <mail@sampersand.me>
2025-01-26 00:29:56 -0800

    mkdir old

--- posix/interactive.sh
+++ posix/interactive.sh
@@ -65,8 +65,30 @@ h () {
 ################################################################################
 
 # Load experimental changes, unless they're disabled.
-if [ -z "$SampShell_no_experimental" ] && [ -e "$SampShell_ROOTDIR/posix/experimental" ]; then
-   . "$SampShell_ROOTDIR/posix/experimental.sh"
+if [ -z "$SampShell_no_experimental" ]; then
+   ping () { curl --connect-timeout 10 "${1:-http://www.example.com}"; }
+
+   alias k+='kill %+'
+
+   ## Creating files
+   alias touchd='mkdir -p' # alias incase i ever end up using it
+
+   ttouch () for file; do  # Same as `touch`, except it will create directories as needed.
+      mkdir -p $file:h && touch $file
+   done
+
+   ## Creating Folders (& cding to them)
+   mkdircd () { mkdir -p $@ && cd $@; }
+   alias cdmkdir=mkdircd
+   alias cdm=mkdircd
+
+   mkd () { mkdir -p $@; }
+   mkf () { mkdir -p ${@:h} && command touch $@; }
+
+   ## Symlinks
+   symlink () {
+      ln -s ${1?need existing file name} ${2?need name of destination}
+   }
 fi
 
 ################################################################################
