f60c5750c7d7f6c0abf56950877477b3cd914aef
SamW <mail@sampersand.me>
2025-03-25 10:45:05 -0700

    updated utils

--- zsh/utils.zsh
+++ zsh/utils.zsh
@@ -1,3 +1,14 @@
+## Shorthand for functions to ensure that the right arguments are given. It's an alias
+# so it shoudl be the first thing. Example:
+#	gs () ARGC_EXACTLY_0 git status
+() {
+	local i
+	for i in {0..9}; do
+		alias ARGC_EXACTLY_$i='${${$(((#=='$i'))&&print x):?exactly '$i' args needed}:#*} '
+		alias ARGC_AT_MOST_$i='${${$(((#<='$i'))&&print x):?at most '$i' args needed}:#*} '
+	done
+}
+
 . ${0:P:h}/functions.zsh
 
 [[ $VENDOR == apple ]] && source ${0:P:h}/macos.zsh
@@ -7,6 +18,8 @@ function ducks {
 }
 # `prp` is a shorthand for `print -P`, which prints out a fmt string as if it were in the prompt.
 function prp { print -P $@ } # NOTE: You can also use `print ${(%)@}`
+
+pwdc () ( ARGC_AT_MOST_1 cd -- "$PWD${1+/$1}" && pbc "$PWD" )
 
 function _SampShell-hg { h | grep $* }
 alias hg='noglob _SampShell-hg'
@@ -24,17 +37,17 @@ alias hd='hexdump -C'
 alias psg='noglob ps -ax | grep '
 
 hr () xx ${@:--}
-hrc () { hr | pbcopy }
+hrc () { ARGC_EXACTLY_0 hr | pbcopy }
 
 ################################################################################
 
-szfiles () subl ~/.z(shenv|shrc|profile|login|logout)
-szrc () subl ~/.zshrc
+szfiles () ARGC_EXACTLY_0 subl ~/.z(shenv|shrc|profile|login|logout)
+szrc () ARGC_EXACTLY_0 subl ~/.zshrc
 
-awkf () awk "BEGIN{$1; exit}"
+awkf () ARGC_EXACTLY_1 awk "BEGIN{$1; exit}"
 +x-exp () +x ~ss/bin/experimental/${^@}
 
-zfns () typeset -m '*_functions'
+zfns () ARGC_EXACTLY_0 typeset -m '*_functions'
 
 function -- -x { typeset +g -x SampShell_XTRACE=1; set -x; "$@" }
 compdef -- _precommand -x
@@ -45,4 +58,4 @@ ufns () {
 	precmd_functions=${precmd_functions:#_SampShell*}
 	zshaddhistory_functions=${zshaddhistory_functions:#_SampShell*}
 }
-asciichars=$'\x00\x01\x02\x03\x04\x05\x06\a\b\t\n\v\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\e\x1C\x1D\x1E\x1F !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9\xBA\xBB\xBC\xBD\xBE\xBF\xC0\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xCA\xCB\xCC\xCD\xCE\xCF\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7\xD8\xD9\xDA\xDB\xDC\xDD\xDE\xDF\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF'
+asciichars=$'\x00\x01\x02\x03\x04\x05\x06\a\b\t\n\v\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9\xBA\xBB\xBC\xBD\xBE\xBF\xC0\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xCA\xCB\xCC\xCD\xCE\xCF\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7\xD8\xD9\xDA\xDB\xDC\xDD\xDE\xDF\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF'
