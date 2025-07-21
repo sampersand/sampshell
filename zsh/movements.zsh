1c9d17e94d19e0a32a2a449e2d9415098f2c3e3b
SamW <mail@sampersand.me>
2025-07-21 14:33:02 +0100

    bugfix forward-kill-argument

--- zsh/movements.zsh
+++ zsh/movements.zsh
@@ -6,11 +6,11 @@ zstyle ':zle:SampShell-*-argument' word-style shell
 autoload -Uz backward-word-match
 autoload -Uz forward-word-match
 autoload -Uz backward-kill-word-match
-autoload -Uz forward-kill-word-match
+autoload -Uz kill-word-match
 zle -N SampShell-backward-argument backward-word-match
 zle -N SampShell-forward-argument forward-word-match
 zle -N SampShell-backward-kill-argument backward-kill-word-match
-zle -N SampShell-forward-kill-argument forward-kill-word-match
+zle -N SampShell-forward-kill-argument kill-word-match
 
 # zle -N SampShell-forward-argument forward-word-match
 bindkey '^[[1;2D' SampShell-backward-argument
