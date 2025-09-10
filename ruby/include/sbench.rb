576f733b24d727a129d4ece92d478d8d80f0338c
SamW <mail@sampersand.me>
2025-09-10 13:35:25 -0700

    comment to sbench

--- ruby/include/sbench.rb
+++ ruby/include/sbench.rb
@@ -17,8 +17,12 @@ def sbench(iterations = 10_000, &block)
   Benchmark.bmbm do |results|
     obj = Object.new
 
-    obj.define_singleton_method(:bench) do |name, &benchblock|
+    obj.define_singleton_method(:bench) do |name=nil, &benchblock|
       raise ArgumentError, 'called without a block', caller(1) unless block_given?
+
+      # Allow users to omit the name, at which point the location is used. If the location is not
+      # possible (maybe it's some C-level function), then just use the block's string
+      name ||= benchblock.source_location&.then{"#{File.basename(_1)}:#{_2}"} || benchblock.to_s
 
       results.report(name) do
         iterations.times(&benchblock)
