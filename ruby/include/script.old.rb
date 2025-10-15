8e273718b09c84a59ab31bad97d7219959370676
SamW <mail@sampersand.me>
2025-10-14 21:12:03 -0700

    update script.rb

---
+++ ruby/include/script.old.rb
@@ -0,0 +1,82 @@
+# Ruby file containing modifications to builtins that I find useful when writing scripts
+
+$PROGRAM_BASE_NAME = File.basename($0, '.*')
+trace_var :$0 do
+  $PROGRAM_BASE_NAME = File.basename($0, '.*')
+end
+
+# Prefix program name to `abort` and `warn`; support `warn()` too
+def abort(message=$!)
+  super("#$PROGRAM_BASE_NAME: #{message}")
+end
+
+def warn(message=$!, ...)
+  super("#$PROGRAM_BASE_NAME: #{message}", ...)
+end
+
+# # Add a `debug()` method for logging
+# def debug(message=nil)
+#   puts("#{PROGRAM_BASE_NAME}: #{message || yield}") if $VERBOSE
+# end
+
+
+alias $ERR_FAIL $-e
+$ERR_FAIL = true
+def `(command)
+  puts command if $-v
+  result = super.chomp
+  if $ERR_FAIL && !$?.success?
+    raise RuntimeError, "command returned #{$?.exitstatus}: #{command}", caller(1)
+  end
+  result
+end
+
+
+
+
+__END__
+return if defined?($SAMPSCRIPT_DISABLED)
+PROGRAM_BASE_NAME = File.basename($0, '.*')
+
+$__progname = $0
+alias $PROGRAM_NAME $0
+alias $0 $__progname
+trace_var :$0 do
+  Process.setproctitle $0
+end
+
+## Always import shell words, and make it easy to use.
+require 'shellwords'
+class String
+  alias ~@ shellescape
+  alias escape shellescape
+end
+
+# Prefix program name to `abort` and `warn`; support `warn()` too
+def abort(message=$!) = super("#{PROGRAM_BASE_NAME}: #{message}")
+def warn(message=$!) = super("#{PROGRAM_BASE_NAME}: #{message}")
+
+# Add a `debug()` method for logging
+def debug(message=nil)
+  puts("#{PROGRAM_BASE_NAME}: #{message || yield}") if $VERBOSE
+end
+
+# Define `failure?` as not success.
+class Process::Status
+  def failure? = !success?
+  alias code exitstatus
+  alias status exitstatus
+end
+
+# Refine the `` ` `` builtin to fail when `$ERR_FAIL` isn't set
+$ERR_FAIL = true unless defined? $ERR_FAIL
+alias $-e $ERR_FAIL # lol, `$-e = true`, like shells
+
+def `(command)
+  debug command
+  result = super.chomp
+  if $ERR_FAIL && $?.failure?
+    raise RuntimeError, "command returned #{$?.exitstatus}: #{command}", caller(1)
+  end
+  result
+end
