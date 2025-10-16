f44f946c67ab656fcdca79fdb48c9707bcfdca59
SamW <mail@sampersand.me>
2025-10-16 11:05:34 -0700

    updated script

--- ruby/include/script.rb
+++ ruby/include/script.rb
@@ -1,30 +1,29 @@
 # Ruby file containing modifications to builtins that I find useful when writing scripts
 
-$PROGRAM_BASE_NAME = File.basename($0, '.*')
-trace_var :$0 do
-  $PROGRAM_BASE_NAME = File.basename($0, '.*')
-end
-
-trace_var :$PROGRAM_NAME do
-  $PROGRAM_BASE_NAME = File.basename($0, '.*')
-end
+$program_name = File.basename $0, '.*'
+trace_var :$0 do $program_name = File.basename($0, '.*') end
+trace_var :$PROGRAM_NAME do $PROGRAM_BASE_NAME = File.basename($0, '.*') end
 
 # Prefix program name to `abort` and `warn`
 def abort(message=$!)
-  super("#$PROGRAM_BASE_NAME: #{message}")
+  super("#{$program_name}: #{message}")
 end
+
 def warn(message, ...)
-  super("#$PROGRAM_BASE_NAME: #{message}", ...)
+  super("#{$program_name}: #{message}", ...)
 end
 
-# Have `foo` raise exceptions
+# Have backticks raise exceptions by default, unless `$BACKTICK_NO_FAIL` is set
 
-$ERR_FAIL = true
-def `(command)
-  puts command if $-v
-  result = super.chomp
-  if $ERR_FAIL && !$?.success?
+$backtick_fail = true
+def shell(command, exception: $backtick_fail)
+  puts "#running: #{command}" if $VERBOSE
+  result = Kernel.`(command) #`
+
+  if exception && !$?.success?
     raise RuntimeError, "command returned #{$?.exitstatus}: #{command}", caller(1)
   end
+
   result
 end
+alias ` shell
