3efb36b9c34d04cd1d84c9d759745416ab41feb7
SamW <mail@sampersand.me>
2026-01-23 12:31:37 -0800

    move movements into keybinds

--- zsh/keybinds.zsh
+++ zsh/keybinds.zsh
@@ -22,10 +22,6 @@ zle -N clear-screen
 #                                               Misc                                               #
 ####################################################################################################
 
-## Useful keybind aliases
-source ~ss/zsh/movements.zsh
-oldw () mv $1 ~ss/old/zsh/widgets
-
 ## Register functions; We use an anonymous function so `fn` doesn't escape
 () { local fn; for fn do zle -N $fn; done } ~ss/zsh/widgets/*(:t)
 
@@ -38,24 +34,21 @@ bindkey '^X^R' redo # like `^X^U` for undo
 bindkey '^[/'    SampShell-delete-path-segment
 # bindkey '^[h'    SampShell-help
 
-bindkey '^[%' SampShell-make-prompt-simple
 bindkey '^[$' SampShell-make-prompt-simple
 
-
-## up and down history, but without going line-by-line
-bindkey '^P' up-history
-bindkey '^N' down-history
+## up and down history, but not line-by-line.
 bindkey '^[^[[A' up-history
+bindkey '^[^[[B' down-history
 
 # Arrow keys that can be used in the future
-bindkey '^[[1;2C' undefined-key # Terminal.app's default sequence for "SHIFT + RIGHT ARROW"
-bindkey '^[[1;2D' undefined-key # Terminal.app's default sequence for "SHIFT + LEFT ARROW"
-bindkey '^[[1;5A' up-history    # (Added as a custom sequence for "CTRL + UP ARROW")
-bindkey '^[[1;5B' down-history  # (Added as a custom sequence for "CTRL + DOWN ARROW")
-bindkey '^[[1;5C' undefined-key # Terminal.app's default sequence for "CTRL + RIGHT ARROW"
-bindkey '^[[1;5D' undefined-key # Terminal.app's default sequence for "CTRL + LEFT ARROW"
-bindkey '^[[H'    undefined-key # TODO: Add into terminal.app as a sequence for `HOME`
-bindkey '^[[E'    undefined-key # TODO: Add into terminal.app as a sequence for `END`
+# bindkey '^[[1;2C' undefined-key # Terminal.app's default sequence for "SHIFT + RIGHT ARROW"
+# bindkey '^[[1;2D' undefined-key # Terminal.app's default sequence for "SHIFT + LEFT ARROW"
+# bindkey '^[[1;5A' up-history    # (Added as a custom sequence for "CTRL + UP ARROW")
+# bindkey '^[[1;5B' down-history  # (Added as a custom sequence for "CTRL + DOWN ARROW")
+# bindkey '^[[1;5C' undefined-key # Terminal.app's default sequence for "CTRL + RIGHT ARROW"
+# bindkey '^[[1;5D' undefined-key # Terminal.app's default sequence for "CTRL + LEFT ARROW"
+# bindkey '^[[H'    undefined-key # TODO: Add into terminal.app as a sequence for `HOME`
+# bindkey '^[[E'    undefined-key # TODO: Add into terminal.app as a sequence for `END`
 
 
 ## TODO: HAVE UP ARROW USE `ZLE_LINE_ABORTED``
@@ -71,7 +64,7 @@ zmodload zsh/deltochar
 function backward-zap-to-char    { zle zap-to-char    -n $(( - ${NUMERIC:-1} )) }
 function backward-delete-to-char { zle delete-to-char -n $(( - ${NUMERIC:-1} )) }
 zle -N backward-zap-to-char
-zle -N backward-delete-to-charcd
+zle -N backward-delete-to-char
 bindkey '^[=' backward-zap-to-char
 bindkey '^[+' backward-delete-to-char
 
@@ -98,3 +91,30 @@ bindkey '^[z' SampShell-put-back-zle
 # ESC + <left/right-arrow> + char = goes to the prev/next instance of `char`
 bindkey '^[^[[D' vi-find-prev-char
 bindkey '^[^[[C' vi-find-next-char
+
+####################################################################################################
+#                               Commands Acting Upon Shell Arguments                               #
+####################################################################################################
+
+autoload -Uz select-word-style
+zstyle ':zle:SampShell-*-argument' word-style shell
+
+autoload -Uz {backward,forward}-word-match
+autoload -Uz {backward-,}kill-word-match
+zle -N SampShell-backward-argument backward-word-match
+zle -N SampShell-forward-argument forward-word-match
+zle -N SampShell-backward-kill-argument backward-kill-word-match
+zle -N SampShell-forward-kill-argument kill-word-match
+
+## ESC+ESC does things based on shell words
+bindkey '^[^[^[[D'  SampShell-backward-argument
+bindkey '^[^[^[[C'  SampShell-forward-argument
+bindkey '^[^[^?'    SampShell-backward-kill-argument
+bindkey '^[^[^[[3~' SampShell-forward-kill-argument
+bindkey '^[^[d'     SampShell-forward-kill-argument
+
+####################################################################################################
+#                                         Bracketed Pastes                                         #
+####################################################################################################
+
+zle -N bracketed-paste SampShell-bracketed-paste
